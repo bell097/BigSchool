8baf37dd53697de01789a846277ad081c6d1e07a
bell097 <nhathoang12102000@gmail.com>
2021-07-01 15:48:36 +0700

    Initial commit

---
+++ BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,45 @@
+﻿using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        private BigSchoolContext db = new BigSchoolContext();
+        private Course obj = new Course();
+        public ActionResult Create()
+        {
+            obj.ListCategory = db.Categories.ToList();
+            return View(obj);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Course obj)
+        {
+            //Không xet LectureId vì bằng user đăng nhập
+            ModelState.Remove("LectureId");
+            if (!ModelState.IsValid)
+            {
+                obj.ListCategory = db.Categories.ToList();
+                return View("Create", obj);
+            }
+            //Lấy login user ID
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            obj.LectureId = user.Id;
+
+            //Add vào CSDL
+            db.Courses.Add(obj);
+            db.SaveChanges();
+
+            //Trở về home, Action Index
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ BigSchool/Models/Course.cs
@@ -0,0 +1,34 @@
+﻿namespace BigSchool.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Course")]
+    public partial class Course
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string LectureId { get; set; }
+
+        [Required(ErrorMessage ="Địa điểm không được để trống")]
+        [StringLength(100, ErrorMessage ="Địa điểm không được quá 100 ký tự")]
+        [Display(Name ="Địa điểm")]
+        public string Place { get; set; }
+
+        [Required(ErrorMessage = "Ngày giờ không được để trống")]
+        [Display(Name = "Ngày giờ")]
+        public DateTime DateTime { get; set; }
+
+        [Required(ErrorMessage = "Môn học không được để trống")]
+        [Display(Name = "Môn học")]
+        public int CategoryId { get; set; }
+
+        public List<Category> ListCategory = new List<Category>();
+        public String Name;
+    }
+}
